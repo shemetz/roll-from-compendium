615187de06d26833d5a2393041e8a144bd7718e8
itamar <itamarcu@gmail.com>
2020-09-20 12:15:14 +0300

    Initial commit (copied from another module)

---
+++ Readme.md
@@ -0,0 +1,53 @@
+# [Zoom/Pan Options](https://foundryvtt.com/packages/zoom-pan-options/)
+
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/itamarcu/ZoomPanOptions?style=for-the-badge) 
+![GitHub Releases](https://img.shields.io/github/downloads/itamarcu/ZoomPanOptions/latest/total?style=for-the-badge) 
+![GitHub All Releases](https://img.shields.io/github/downloads/itamarcu/ZoomPanOptions/total?style=for-the-badge&label=Downloads+total)  
+
+FoundryVTT module to change mouse scroll behavior. It has several settings, which can be toggled individually, and stored locally (per client).
+
+To install, browse for it in the module browser, or [directly copy the manifest link for the latest release](https://github.com/itamarcu/ZoomPanOptions/releases/latest/download/module.json).
+
+# Features
+
+## Zoom around cursor
+- When zooming in and out, the camera will stay focused on the cursor, like in many other applications.
+- This does not affect PageUp, PageDown, or Numpad +/-. Those will still zoom focused on the center of the screen.
+
+## Disable zoom rounding
+- Disables default Foundry behavior, which rounds zoom to the nearest 1%. Will make zooming smoother, especially for touchpad users.
+ 
+## "Default" mode
+- Same as the Foundry behavior
+
+## "Touchpad" mode
+- Pan with two-finger drag on the touchpad.
+- Zoom with two-finger pinch or Ctrl+scroll.
+- Rotate with Shift+scroll and Ctrl+Shift+scroll.
+- (Ctrl can be replaced with Cmd (mac) or WinKey (windows), as usual in Foundry)
+
+## "Alternative" mode
+- Pan with touchpad, or with mouse: vertical mouse scroll will pan up and down, and horizontal mouse scroll will pan left and right.
+- Zoom with two-finger pinch or Ctrl+scroll.
+- Rotate with Alt+Shift+scroll and Alt+Ctrl+scroll.
+- (Ctrl can be replaced with Cmd (mac) or WinKey (windows), as usual in Foundry)
+
+## Pan speed multiplier
+- Only used in touchpad and alternative modes. Multiplies pan speed. Defaults to 1, which should be close to the pan speed when right-click-dragging the canvas.
+
+## Zoom speed multiplier
+- Useful if your zoom is too sensitive, or not sensitive enough.
+- Set to 0 for default Foundry behavior (5% zoom per mouse tick, always).
+- Set to 1 for zooming based on scroll delta, which should be similar to default zoom for most common mouse types.
+- Set to 0.1 for slower zooming, or 10 for faster zooming.
+- Technically you can give this a negative value to flip your zoom directions, if you're an oddball. 
+
+# Credits
+
+Thanks to mrkwnzl#7407 for the touchpad support and testing!
+
+Thanks to akrigline/Calego for merging with CanvasScroll and helping with libwrapper and workflows! 
+
+# [Cursor Zoom](https://github.com/itamarcu/CursorZoom)
+My old foundry module, that only had the "Zoom around cursor" feature, and did not allow configuring it in the settings for each player.
+If you were a user of Cursor Zoom, please uninstall it and install this module. Your players will be able to each set their own preference.
---
+++ scripts/zoom-pan-options.js
@@ -0,0 +1,216 @@
+import { libWrapper } from './libwrapper-shim.js'
+
+const MODULE_ID = 'zoom-pan-options'
+
+function getSetting (settingName) {
+  return game.settings.get(MODULE_ID, settingName)
+}
+
+/**
+ * (note: return value is meaningless here)
+ */
+function _onWheel_Override (event) {
+  const mode = getSetting('pan-zoom-mode')
+  const shift = event.shiftKey
+  const alt = event.altKey
+  const ctrlOrMeta = event.ctrlKey || event.metaKey  // meta key (cmd on mac, winkey in windows) will behave like ctrl
+
+  // Prevent zooming the entire browser window
+  if (ctrlOrMeta) {
+    event.preventDefault()
+  }
+
+  // Handle wheel events for the canvas if it is ready and if it is our hover target
+  let hover = document.elementFromPoint(event.clientX, event.clientY)
+  if (!(canvas && canvas.ready && hover && hover.id === 'board')) return null
+
+  event.preventDefault()
+  const layer = canvas.activeLayer
+
+  // Case 1 - rotate stuff
+  if (layer instanceof PlaceablesLayer) {
+    if (mode === 'Default' && (ctrlOrMeta || shift)) {
+      return layer._onMouseWheel(event)
+    }
+    if (mode === 'Touchpad' && shift) {
+      return layer._onMouseWheel({
+        deltaY: event.wheelDelta, // only the sign matters, and we'll use wheelDelta instead of relying on deltaY
+        shiftKey: shift && !ctrlOrMeta,
+      })
+    }
+    if (mode === 'Alternative' && alt && (ctrlOrMeta || shift)) {
+      return layer._onMouseWheel({
+        deltaY: event.wheelDelta, // only the sign matters, and we'll use wheelDelta instead of relying on deltaY
+        shiftKey: shift,
+      })
+    }
+  }
+
+  // Case 2 - zoom the canvas
+  // (written to be readable)
+  if (
+    mode === 'Default'
+    || (mode === 'Touchpad' && ctrlOrMeta)
+    || (mode === 'Alternative' && ctrlOrMeta)
+  ) {
+    return zoom(event)
+  }
+
+  // Cast 3 - pan the canvas horizontally (shift+scroll)
+  if (mode === 'Alternative' && shift) {
+    // noinspection JSSuspiciousNameCombination
+    return panWithMultiplier({
+      deltaX: event.deltaY,
+    })
+  }
+
+  // Case 4 - pan the canvas in the direction of the mouse/touchpad event
+  panWithMultiplier(event)
+}
+
+function _constrainView_Override ({ x, y, scale } = {}) {
+  const d = canvas.dimensions
+
+  // Constrain the maximum zoom level
+  if (Number.isNumeric(scale) && scale !== canvas.stage.scale.x) {
+    const max = CONFIG.Canvas.maxZoom
+    const ratio = Math.max(d.width / window.innerWidth, d.height / window.innerHeight, max)
+    // override changes are just for this part:
+    if (getSetting('disable-zoom-rounding')) scale = Math.clamped(scale, 1 / ratio, max)
+    else scale = Math.round(Math.clamped(scale, 1 / ratio, max) * 100) / 100
+  } else {
+    scale = canvas.stage.scale.x
+  }
+
+  // Constrain the pivot point using the new scale
+  if (Number.isNumeric(x) && x !== canvas.stage.pivot.x) {
+    const padw = 0.4 * (window.innerWidth / scale)
+    x = Math.clamped(x, -padw, d.width + padw)
+  } else x = canvas.stage.pivot.x
+  if (Number.isNumeric(y) && x !== canvas.stage.pivot.y) {
+    const padh = 0.4 * (window.innerHeight / scale)
+    y = Math.clamped(y, -padh, d.height + padh)
+  } else y = canvas.stage.pivot.y
+
+  // Return the constrained view dimensions
+  return { x, y, scale }
+}
+
+/**
+ * Will zoom around cursor, and based on delta.
+ */
+function zoom (event) {
+  const multiplier = getSetting('zoom-speed-multiplier')
+  let dz = -event.deltaY * 0.0005 * multiplier + 1
+  // default foundry behavior if zoom-speed-multiplier is 0
+  if (multiplier === 0) dz = event.deltaY < 0 ? 1.05 : 0.95
+
+  if (!getSetting('zoom-around-cursor')) {
+    canvas.pan({ scale: dz * canvas.stage.scale.x })
+    return
+  }
+
+  const scale = dz * canvas.stage.scale.x
+  const d = canvas.dimensions
+  const max = CONFIG.Canvas.maxZoom
+  const min = 1 / Math.max(d.width / window.innerWidth, d.height / window.innerHeight, max)
+
+  if (scale > max || scale < min) {
+    canvas.pan({ scale: scale > max ? max : min })
+    console.log('Zoom/Pan Options |', `scale limit reached (${scale}).`)
+    return
+  }
+
+  // Acquire the cursor position transformed to Canvas coordinates
+  const t = canvas.stage.worldTransform
+  const dx = ((-t.tx + event.clientX) / canvas.stage.scale.x - canvas.stage.pivot.x) * (dz - 1)
+  const dy = ((-t.ty + event.clientY) / canvas.stage.scale.y - canvas.stage.pivot.y) * (dz - 1)
+  const x = canvas.stage.pivot.x + dx
+  const y = canvas.stage.pivot.y + dy
+  canvas.pan({ x, y, scale })
+}
+
+function panWithMultiplier (event) {
+  const multiplier = (1 / canvas.stage.scale.x) * getSetting('pan-speed-multiplier')
+  const x = canvas.stage.pivot.x + event.deltaX * multiplier
+  const y = canvas.stage.pivot.y + event.deltaY * multiplier
+  canvas.pan({ x, y })
+}
+
+Hooks.on('init', function () {
+  console.log('Initializing Zoom/Pan Options')
+  game.settings.register(MODULE_ID, 'zoom-around-cursor', {
+    name: 'Zoom around cursor',
+    hint: 'Center zooming around cursor. Does not apply to zooming with pageup or pagedown.',
+    scope: 'client',
+    config: true,
+    default: true,
+    type: Boolean,
+  })
+  game.settings.register(MODULE_ID, 'pan-zoom-mode', {
+    name: 'Pan/Zoom Mode',
+    hint: `
+      Default: Standard foundry behavior. Zoom with mouse scroll. Rotate with Shift+scroll and Ctrl+scroll.
+||
+      Touchpad: Pan with two-finger drag. Zoom with two-finger pinch or Ctrl+scroll. Rotate with Shift+scroll and Ctrl+Shift+scroll.
+||
+      Alternative: Pan with two-finger drag or scroll or shift+scroll. Zoom with two-finger pinch or Ctrl+scroll. Rotate with Alt+Shift+scroll and Alt+Ctrl+scroll.
+    `,
+    scope: 'client',
+    config: true,
+    type: String,
+    choices: {
+      'Default': 'Default: standard foundry behavior',
+      'Touchpad': 'Touchpad: drag, pinch, rotate with Shift or Ctrl+Shift',
+      'Alternative': 'Alternative: can pan with Shift, rotate while holding Alt',
+    },
+    default: 'Default',
+  })
+  game.settings.register(MODULE_ID, 'disable-zoom-rounding', {
+    name: 'Disable zoom rounding',
+    hint:
+      'Disables default Foundry behavior, which rounds zoom to the nearest 1%. Will make zooming smoother, especially for touchpad users.',
+    scope: 'client',
+    config: true,
+    default: true,
+    type: Boolean,
+  })
+  game.settings.register(MODULE_ID, 'zoom-speed-multiplier', {
+    name: 'Zoom speed',
+    hint:
+      'Multiplies zoom speed, affecting scaling speed. 0.1 or 10 might be better for some touchpads. 0 for default Foundry behavior.',
+    scope: 'client',
+    config: true,
+    default: 0,
+    type: Number,
+  })
+  game.settings.register(MODULE_ID, 'pan-speed-multiplier', {
+    name: 'Pan speed',
+    hint:
+      'Multiplies pan speed. Defaults to 1, which should be close to the pan speed when right-click-dragging the canvas.',
+    scope: 'client',
+    config: true,
+    default: 1,
+    type: Number,
+  })
+})
+
+Hooks.once('setup', function () {
+  libWrapper.register(
+    MODULE_ID,
+    'KeyboardManager.prototype._onWheel',
+    (event) => {
+      return _onWheel_Override(event)
+    },
+    'OVERRIDE'
+  )
+  libWrapper.register(
+    MODULE_ID,
+    'Canvas.prototype._constrainView',
+    (obj) => {
+      return _constrainView_Override(obj)
+    },
+    'OVERRIDE' // only overrides a tiny part of the function... would be nice if foundry made it more modular
+  )
+  console.log('Done setting up Zoom/Pan Options.')
+})
